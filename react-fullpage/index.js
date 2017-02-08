fff85c9174b2035878bf5132738a9cc8e9b9be6a
wiliam qiu <weiyang.qiu@ctauto.cn>
2017-02-08 10:52:18 +0800

    commit react-fullpage

---
+++ react-fullpage/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SectionsContainer: require('./dist/SectionsContainer').default,
+  Section: require('./dist/Section').default,
+  Header: require('./dist/Header').default,
+  Footer: require('./dist/Footer').default,
+};
